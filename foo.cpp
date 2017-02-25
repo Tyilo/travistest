cfac9bcb2c7afd21dd61489172a1f09597ab0f62
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2017-02-25 17:44:57 +0100

    update

--- foo.cpp
+++ foo.cpp
@@ -1,5 +1,13 @@
+#include <iostream>
 #include <type_traits>
 
 int main() {
+#if defined(__clang__)
+	std::cout << "clang\n";
+#elif defined(__GNUC__) || defined(__GNUG__)
+	std::cout << "gcc\n";
+#else
+	std::cout << "unknown\n";
+#endif
 	return !std::is_trivially_copyable<int>::value;
 }
